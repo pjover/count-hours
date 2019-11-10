fb04d1d40fd202d6039b1d47ad17eec8ff64bfe6
Pere Jover <pjover@hotelbeds.com>
2019-11-10 17:17:24 +0100

    count_hours script

--- CountHours.py
+++ CountHours.py
@@ -1,11 +1,8 @@
-#! /usr/bin/env python3
-import argparse
+
 import logging
 import re
 import sys
 from datetime import datetime
-
-from FileManager import FileManager
 
 
 def read_current_month(_line):
@@ -57,80 +54,67 @@ def read_calculated_hours(_line):
         return f
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", type=str, help="The Markdown file to process")
-    parser.add_argument("-bf", "--backup_folder", type=str, help="Folder to store the backups", default='log_backups')
-    parser.add_argument("-d", "--debug", help="Shows all the DEBUG level logging messages", action='store_true')
-    args = parser.parse_args()
-    if args.debug:
-        logging.getLogger().setLevel(logging.DEBUG)
-        logging.info('Showing all the DEBUG level logging messages')
-    else:
-        logging.getLogger().setLevel(logging.INFO)
-    return args
+class MarkdownProcessor(object):
+    _modified_lines = 0
 
+    def process(self, lines: list) -> list:
+        logging.debug('Processing %d lines', len(lines))
+        self._modified_lines = 0
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    file_manager = FileManager(args.file_path, args.backup_folder)
+        months = {}
+        current_month = None
+        summary_line_index = len(lines)
+        for i, line in enumerate(lines):
+            if line.startswith('# Summary'):
+                summary_line_index = i
+                break
 
-    lines = file_manager.load()
+            elif line.startswith('## Date: '):
+                # Llegeix el mes
+                current_month = read_current_month(line)
 
-    months = {}
-    current_month = None
-    modified_lines = 0
-    summary_line_index = len(lines)
-    for i, line in enumerate(lines):
-        if line.startswith('# Summary'):
-            summary_line_index = i
-            break
+            elif line.startswith('Hours: ?'):
+                # Llegeix ses hores sense calcular
+                total_time = read_non_calculated_hours(line)
+                if not total_time:
+                    sys.exit('Error in line format "{0}"'.format(line))
 
-        elif line.startswith('## Date: '):
-            # Llegeix el mes
-            current_month = read_current_month(line)
-
-        elif line.startswith('Hours: ?'):
-            # Llegeix ses hores sense calcular
-            total_time = read_non_calculated_hours(line)
-            if not total_time:
-                sys.exit('Error in line format "{0}"'.format(line))
-
-            # Convertim el temps a format decimal
-            hexa_time = str(total_time)
-            splitted_time = hexa_time.split(':')
-            decimal_time = int(splitted_time[0]) + (int(splitted_time[1]) / 60)
-            formatted_time = '{0} [{1:0.2f} h]'.format(hexa_time, decimal_time)
-            logging.info('Added {0:0.2f} Hours'.format(decimal_time))
-
-            # Actualitza ses hores del mes en curs
-            months[current_month] = months.get(current_month, 0) + decimal_time
-
-            # Substitueix el simbol ? per el temps calculat
-            modified_lines += 1
-            lines[i] = line.replace('?', formatted_time)
-
-        elif line.startswith('Hours: '):
-            # Llegeix ses hores ja calculades per al resum
-            decimal_time = read_calculated_hours(line)
-
-            if decimal_time is None:
-                sys.exit('Error in line format "{0}"'.format(line))
-            else:
-                logging.info("There were already {0:0.2f} hours".format(decimal_time))
+                # Convertim el temps a format decimal
+                hexa_time = str(total_time)
+                splitted_time = hexa_time.split(':')
+                decimal_time = int(splitted_time[0]) + (int(splitted_time[1]) / 60)
+                formatted_time = '{0} [{1:0.2f} h]'.format(hexa_time, decimal_time)
+                logging.info('Added {0:0.2f} Hours'.format(decimal_time))
 
                 # Actualitza ses hores del mes en curs
                 months[current_month] = months.get(current_month, 0) + decimal_time
 
-    # Escriu el resumen
-    lines = lines[: summary_line_index]
-    lines.append('# Summary:\n')
-    for month in months:
-        msg = '{0} = {1:0.2f} h'.format(month, months.get(month, 0))
-        logging.info(msg)
-        lines.append('\t- {0}\n'.format(msg))
+                # Substitueix el simbol ? per el temps calculat
+                self._modified_lines += 1
+                lines[i] = line.replace('?', formatted_time)
 
-    if not modified_lines:
-        logging.info("No changes to process")
-    else:
-        file_manager.save(lines)
+            elif line.startswith('Hours: '):
+                # Llegeix ses hores ja calculades per al resum
+                decimal_time = read_calculated_hours(line)
+
+                if decimal_time is None:
+                    sys.exit('Error in line format "{0}"'.format(line))
+                else:
+                    logging.info("There were already {0:0.2f} hours".format(decimal_time))
+
+                    # Actualitza ses hores del mes en curs
+                    months[current_month] = months.get(current_month, 0) + decimal_time
+
+        # Escriu el resumen
+        lines = lines[: summary_line_index]
+        lines.append('# Summary:\n')
+        for month in months:
+            msg = '{0} = {1:0.2f} h'.format(month, months.get(month, 0))
+            logging.info(msg)
+            lines.append('\t- {0}\n'.format(msg))
+
+        return lines
+
+    def has_changed(self) -> bool:
+        logging.debug('There are %d modified lines', self._modified_lines)
+        return self._modified_lines > 0
---
+++ count_hours.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import argparse
+import logging
+
+from CountHours import MarkdownProcessor
+from FileManager import FileManager
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=str, help="The Markdown file to process")
+    parser.add_argument("-bf", "--backup_folder", type=str, help="Folder to store the backups", default='log_backups')
+    parser.add_argument("-d", "--debug", help="Shows all the DEBUG level logging messages", action='store_true')
+    args = parser.parse_args()
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.info('Showing all the DEBUG level logging messages')
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    file_manager = FileManager(args.file_path, args.backup_folder)
+    processor = MarkdownProcessor()
+    lines = processor.process(file_manager.load())
+
+    if processor.has_changed():
+        file_manager.save(lines)
+    else:
+        logging.info("No changes to process")
