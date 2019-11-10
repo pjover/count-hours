d6e42aa499f733a0836ae8744dd8418acab8d35e
Pere Jover <pjover@hotelbeds.com>
2019-11-10 19:22:15 +0100

    MarkdownProcessor refactor

--- MarkdownProcessor.py
+++ MarkdownProcessor.py
@@ -1,6 +1,5 @@
 import logging
 import re
-import sys
 from datetime import datetime
 
 
@@ -11,46 +10,6 @@ class MarkdownProcessor:
     _parse_non_calculated_hours_segment_re = re.compile(r'([0-2][0-9]:[0-5][0-9])-([0-2][0-9]:[0-5][0-9])')
     _parse_calculated_hours_re = re.compile(r'\[([.0-9]*)[ h\]]')
 
-    def parse_current_month(self, line):
-        result = self._parse_current_month_re.search(line)
-        return result.group(1)
-
-    def parse_non_calculated_hours(self, line):
-        _total_time = None
-
-        result = self._parse_non_calculated_hours_segments_re.search(line)
-        if not result:
-            return None
-
-        else:
-            segments = result.group(1).split('+')
-            logging.debug('Found: %s', segments)
-            for segment in segments:
-                result = self._parse_non_calculated_hours_segment_re.search(segment)
-                if not result:
-                    return None
-
-                start = result.group(1)
-                end = result.group(2)
-                delta_time = datetime.strptime(end, '%H:%M') - datetime.strptime(start, '%H:%M')
-                logging.debug('\tStart: %s\tEnd: %s\tDifference: %s', start, end, delta_time)
-                if _total_time:
-                    _total_time = _total_time + delta_time
-                else:
-                    _total_time = delta_time
-
-        return _total_time
-
-    def parse_calculated_hours(self, line):
-        result = self._parse_calculated_hours_re.search(line)
-        if not result:
-            return None
-
-        else:
-            f = float(result.group(1))
-            logging.debug('Found: %f', f)
-            return f
-
     def process(self, lines: list) -> list:
         logging.debug('Processing %d lines', len(lines))
         self._modified_lines = 0
@@ -59,55 +18,107 @@ class MarkdownProcessor:
         current_month = None
         summary_line_index = len(lines)
         for i, line in enumerate(lines):
-            if line.startswith('# Summary'):
+            if self._is_summary(line):
                 summary_line_index = i
                 break
 
-            elif line.startswith('## Date: '):
-                # Llegeix el mes
+            elif self._is_date(line):
                 current_month = self.parse_current_month(line)
 
-            elif line.startswith('Hours: ?'):
-                # Llegeix ses hores sense calcular
-                total_time = self.parse_non_calculated_hours(line)
-                if not total_time:
-                    sys.exit('Error in line format "{0}"'.format(line))
+            elif self._are_non_calculated_hours(line):
+                (str_time, decimal_time) = self._get_time(line)
+                lines[i] = line.replace('?', self._format_time(str_time, decimal_time))
+                months[current_month] = months.get(current_month, 0) + decimal_time
+                self._modified_lines += 1
 
-                # Convertim el temps a format decimal
-                hexa_time = str(total_time)
-                splitted_time = hexa_time.split(':')
-                decimal_time = int(splitted_time[0]) + (int(splitted_time[1]) / 60)
-                formatted_time = '{0} [{1:0.2f} h]'.format(hexa_time, decimal_time)
-                logging.info('Added {0:0.2f} Hours'.format(decimal_time))
-
-                # Actualitza ses hores del mes en curs
+            elif self._are_calculated_hours(line):
+                decimal_time = self.parse_calculated_hours(line)
                 months[current_month] = months.get(current_month, 0) + decimal_time
 
-                # Substitueix el simbol ? per el temps calculat
-                self._modified_lines += 1
-                lines[i] = line.replace('?', formatted_time)
-
-            elif line.startswith('Hours: '):
-                # Llegeix ses hores ja calculades per al resum
-                decimal_time = self.parse_calculated_hours(line)
-
-                if decimal_time is None:
-                    sys.exit('Error in line format "{0}"'.format(line))
-                else:
-                    logging.info("There were already {0:0.2f} hours".format(decimal_time))
-
-                    # Actualitza ses hores del mes en curs
-                    months[current_month] = months.get(current_month, 0) + decimal_time
-
-        # Escriu el resumen
         lines = lines[: summary_line_index]
-        lines.append('# Summary:\n')
+        lines = lines + self._summary_lines(months)
+        return lines
+
+    @staticmethod
+    def _is_date(line: str) -> bool:
+        return line.startswith('## Date: ')
+
+    @staticmethod
+    def _is_summary(line: str) -> bool:
+        return line.startswith('# Summary')
+
+    def parse_current_month(self, line):
+        result = self._parse_current_month_re.search(line)
+        return result.group(1)
+
+    @staticmethod
+    def _are_non_calculated_hours(line: str) -> bool:
+        return line.startswith('Hours: ?')
+
+    def _get_time(self, line: str):
+        total_time = self.parse_non_calculated_hours(line)
+        if not total_time:
+            raise Exception('Error in line format "{0}"'.format(line))
+
+        str_time = str(total_time)
+        time_parts = str_time.split(':')
+        decimal_time = int(time_parts[0]) + (int(time_parts[1]) / 60)
+        logging.info('Added {0:0.2f} Hours'.format(decimal_time))
+        return str_time, decimal_time
+
+    def parse_non_calculated_hours(self, line):
+        _total_time = None
+        result = self._parse_non_calculated_hours_segments_re.search(line)
+        if not result:
+            return None
+
+        else:
+            segments = result.group(1).split('+')
+            logging.debug('Found: %s', segments)
+            for segment in segments:
+                delta_time = self._calculate_delta_time(segment)
+                if _total_time:
+                    _total_time = _total_time + delta_time
+                else:
+                    _total_time = delta_time
+
+        return _total_time
+
+    def _calculate_delta_time(self, segment):
+        result = self._parse_non_calculated_hours_segment_re.search(segment)
+        if not result:
+            return None
+
+        start = result.group(1)
+        end = result.group(2)
+        delta_time = datetime.strptime(end, '%H:%M') - datetime.strptime(start, '%H:%M')
+        logging.debug('\tStart: %s\tEnd: %s\tDelta: %s', start, end, delta_time)
+        return delta_time
+
+    @staticmethod
+    def _format_time(str_time, decimal_time) -> str:
+        return '{0} [{1:0.2f} h]'.format(str_time, decimal_time)
+
+    @staticmethod
+    def _are_calculated_hours(line: str) -> bool:
+        return line.startswith('Hours: ')
+
+    def parse_calculated_hours(self, line) -> float:
+        result = self._parse_calculated_hours_re.search(line)
+        if not result:
+            raise Exception('Error in line format "{0}"'.format(line))
+        decimal_time = float(result.group(1))
+        logging.info("There were already {0:0.2f} hours".format(decimal_time))
+        return decimal_time
+
+    @staticmethod
+    def _summary_lines(months):
+        new_lines = ['# Summary:\n']
         for month in months:
             msg = '{0} = {1:0.2f} h'.format(month, months.get(month, 0))
+            new_lines.append('- {0}\n'.format(msg))
             logging.info(msg)
-            lines.append('- {0}\n'.format(msg))
-
-        return lines
+        return new_lines
 
     def has_changed(self) -> bool:
         logging.debug('There are %d modified lines', self._modified_lines)
